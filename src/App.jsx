9492276e3086c59cb5872528e6cba9202d93a18b
Nirmal-Chamara <kpnirmalchamara@gmail.com>
2026-05-16 11:01:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div classname="min-h-screen bg-slate-950 text-white overflow-hidden ">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
